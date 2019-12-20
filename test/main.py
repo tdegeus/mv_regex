ef4d89af8b12cb319168a2304bd854aa5e56f366
Tom de Geus <tom@geus.me>
2019-12-20 19:10:26 +0100

    Fixing CI

--- test/main.py
+++ test/main.py
@@ -1,12 +1,6 @@
 
 import subprocess
 import os
-import sys
-
-if sys.platform.startswith('win'):
-    command = 'mv-regex'
-else:
-    command = 'mv_regex'
 
 # support function
 # ----------------
@@ -20,7 +14,7 @@ def run(cmd):
 open('test1_foo.log', 'w').write('foo')
 open('test1_bar.log', 'w').write('bar')
 
-run(command + r' -f "(.*)(\.log)" "\1\2.bak" *')
+run(r'mv_regex -f "(.*)(\.log)" "\1\2.bak" *')
 
 assert not os.path.isfile('test1_foo.log')
 assert not os.path.isfile('test1_bar.log')
